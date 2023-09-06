55cd908c2a0da779dd363ceaa6c5912845c39878
Sorin022 <97994615+SorinMac@users.noreply.github.com>
2023-09-06 12:44:27 -0400

    9/06/23 main file reader set up and start of hpp

--- Assignment 1/Functions1.hpp
+++ Assignment 1/Functions1.hpp
@@ -1,3 +1,45 @@
 #include <iostream>
 using namespace std;
 
+struct Node{
+    char data;
+    Node* link;
+};
+
+typedef Node* ptr;
+
+char pop() {
+
+}
+
+void push() {
+
+}
+
+boolean isEmptyStack() {
+
+}
+
+struct Queue{
+    Node* front;
+    Node* back;
+
+    Queue(){
+        front = NULL;
+        back =  NULL;
+    }
+}
+
+void EnQueue(){
+
+}
+
+char DeQueue(){
+
+}
+
+boolean isEmptyQueue() {
+
+}
+
+
