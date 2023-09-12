86da46ea9abdca60836911e01e065ddd6ac11fad
SorinMac <sorin.macaluso1@marist.edu>
2023-09-12 13:16:56 -0400

    9/12/23

--- Assignment 1/main.cpp
+++ Assignment 1/main.cpp
@@ -1,7 +1,7 @@
 //first two are librarys
 #include <iostream> //object oriented library that allows input and output using streams
 #include <fstream> //allows for the reading of a file in the library
-#include <string>
+#include <string> /* These three are used for the removing of a space for the strings */
 #include <algorithm>
 #include <cctype>
 #include "Functions1.hpp" //all the functions that will be used to make the node, stack, queue, and the sorting
@@ -25,8 +25,10 @@ int main(){
 
     //checks if the file is open
     if (File.is_open()){
+
         //while file is open gets the line
         while (File.good()){
+
             getline(File, readInString);
 
             //this for loop will make is so that all spaces and special characters are all one specific symbol to check for
@@ -37,6 +39,7 @@ int main(){
                 }
             }
 
+            //uses three of the includes (string, algorithms, cctype) to remove the space
              readInString.erase(std::remove_if(readInString.begin(), readInString.end(), ::isspace),
              readInString.end());
 
@@ -74,25 +77,32 @@ int main(){
             queue.EnQueue(PalanCheck[k]);
         }
 
+        //pops the char out of the stack and dequeus it at the same time
         for (int j = 0; j < PalanCheck.length(); j++){
             char popChar =  pop(*&head);
             char queueChar =  queue.DeQueue();
 
+            //checks if they match
             if (popChar != queueChar){
+
+                //count is used to make sure the programm checks the whole word (rest cause some words have a few that match)
                 count = 0;
                 break;
+
             } else if (popChar = queueChar){
+
+                //if the two do match adds to count
                 count++;
 
+                //if the count equal the lenght of the string (did the whole string so print it out)
                 if(count == PalanCheck.length()){
                     cout << PalanCheck << "\n";
                     count = 0;
                 }
 
             }
+
         }
-
-
 
     }
 
