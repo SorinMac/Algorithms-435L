1a8eb8343ae227e7ce9d0314cdbf73be60db6de5
SorinMac <sorin.macaluso1@marist.edu>
2023-10-29 10:12:12 -0400

    10/29/23

---
+++ Assignment3/BST.hpp
@@ -0,0 +1,56 @@
+//librarys that are always used in c++
+#include <iostream>
+using namespace std;
+
+//This class will be the nodes that are made to make the link list
+//The data type for data is char since there will characters passed to make the list
+struct BST{
+    string data;
+    BST* left = nullptr;
+    BST* right = nullptr;
+    BST* parent = nullptr;
+};
+
+string BSTTreeInsert(BST*& root, string value){
+    BST* trailing = nullptr;
+    BST* current = root;
+    string path = "";
+
+    BST* node = new BST;
+    node->data = value;
+
+    while(current != nullptr){
+        trailing = current;
+        if(node->data < current->data){
+            current = current->left; // have to make current the node that needs to be checked for
+            path = path + "L"; 
+        }else{
+            current = current->right; // have to make current the node that needs to be checked for
+            path = path + "R";
+        }
+    }
+
+    node->parent = trailing;
+    if(trailing == nullptr){
+        root = node;
+    }else{
+        if(node->data < trailing->data){
+            trailing->left = node; // check if these are working too seems not really since the stuff up there is not
+        }else{
+            trailing->right = node; // check if these are working too seems not really since the stuff up there is not
+        }
+    }
+
+    return path;
+
+}
+
+BST* BSTSearch(BST* node, string key){
+    if(node->left == nullptr || node->left == nullptr || node->data == key){
+        return node;
+    }else if(key < node->data){
+        return BSTSearch(node->left, key);
+    }else{
+        return BSTSearch(node->right, key);
+    }
+}
---
+++ Assignment3/main.cpp
@@ -0,0 +1,62 @@
+//first two are librarys
+#include <iostream> //object oriented library that allows input and output using streams
+#include <fstream> //allows for the reading of a file in the library
+#include <string> /* These three are used for the removing of a space for the strings */
+#include <algorithm> /* These three are used for the removing of a space for the strings */
+#include <cctype> /* These three are used for the removing of a space for the strings */
+#include <iomanip> //used to set the amount of accuracy for the decmial points
+#include "BST.hpp"
+
+using namespace std;
+
+const int arraysize = 666;
+int palanplacecheck = 1;
+
+//main functions
+int main(){
+    
+    cout << "\n";
+
+    int count = 0;
+
+    int magicIteamCount = 0;
+    string magicIteam[arraysize];
+
+    //start of the file stream studd
+    string readInString;
+
+    //opens the right file
+    ifstream File ("magicitems.txt");
+
+    //checks if the file is open
+    if (File.is_open()){
+
+        //while file is open gets the line
+        while (File.good()){
+
+            getline(File, readInString);
+
+            //add the now properly formatted line to the array
+            magicIteam[magicIteamCount] = readInString;
+            magicIteamCount++;
+
+            
+        }
+        //closed the file at the end when all done
+        File.close();
+    }
+
+    //error checking if the file is not opened
+    else cout << "Unable to open file"; 
+
+    BST* root;
+    root = new BST;
+
+    for(int i = 0; i < arraysize; i++){
+        string insertPath = BSTTreeInsert(root , magicIteam[i]);
+        cout << "This is the path of " + magicIteam[i] + " " + insertPath + "." << "\n";
+    }
+
+
+}
+
