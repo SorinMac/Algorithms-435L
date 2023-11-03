a20c5caa6f60bc6735cc6a256d2a19b0782f8e6c
SorinMac <sorin.macaluso1@marist.edu>
2023-11-02 20:53:55 -0400

    11/2/23

--- Assignment3/Matrix.hpp
+++ Assignment3/Matrix.hpp
@@ -5,10 +5,17 @@ using namespace std;
 
 void MatrixGraph(int vertexs, vector<int> start, vector<int> end) {
     int Matrix[vertexs][vertexs];
+    const int VECTOR_SIZE = start.size();
 
-    for(int i = 0; i < start.size() && i < end.size(); i++){
-        Matrix[start[i]][end[i]] = 1;
-        Matrix[end[i]][start[i]] = 1;
+    for(int i = 0; i < vertexs; i++){
+        for(int j = 0; j < vertexs; j++){
+            Matrix[i][j] = 0;
+        }
     }
+
+    for(int i = 0; i < VECTOR_SIZE; i++){
+        Matrix[start[i]-1][end[i]-1] = 1;
+        Matrix[end[i]-1][start[i]-1] = 1;
+    } 
 
 }
