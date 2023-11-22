482fb9ebe97666fcec925c9b1ba35a29423bc1b9
SorinMac <sorin.macaluso1@marist.edu>
2023-11-22 11:05:45 -0500

    11/22/23

--- Assignment4/FractionalGreedy.hpp
+++ Assignment4/FractionalGreedy.hpp
@@ -10,62 +10,100 @@
 
 using namespace std;
 
+//this is the spice class that i made 
 struct spice{
+    //name of the spice
     string name;
+    //the price as a float if needed
     float price;
+    //the QTy
     int QTY;
+    //unitPrice to be caculated
     float unitPrice;
+    //processed (taken from Assignment3) so that we do not indefinetly check the same spice
+    bool Processed;
 };
 
+//this will hold all the different spice's
 vector<spice*> SpiceHolder;
 
 void Spice(string SpiceName, float SpicePrice, int SpiceQTY){
 
+    //creates a new spice 
     spice* Spice = new spice;
+
+    //uses data to populate the parts of the class
     Spice->name = SpiceName;
     Spice->price = SpicePrice;
     Spice->QTY = SpiceQTY;
     Spice->unitPrice = SpicePrice/SpiceQTY;
+    Spice->Processed = false;
 
+    //then adds the pointer to the spice to the SpiceHolder vector so that i can access to them all
     SpiceHolder.push_back(Spice);
     
 }
 
+//Math ✅
+//Print out ❎
+
 
 void FractionalGreedy(int size){
-    int NapSize = 0;
-    int condition = 0;
+    //is given the size of the knap sack
+    int NapSize = size;
+    //consant for the print out
+    const int trueSize =  size;
+    //to keep hold of the total
     float total = 0;
-    spice* GUnitPrice;
-    GUnitPrice->unitPrice = 0;
 
-    NapSize = size;
+    //will go until the Knap Sack is 0 in size
+    while(NapSize > 0){
+        //create a new pointer to find the greatest unit price
+        spice* GUnitPrice = nullptr;
 
-    for(spice* j : SpiceHolder){
-
-        if(j->unitPrice > GUnitPrice->unitPrice){
-
-            GUnitPrice = j;
-
+        //goes through all the spices that are in the spice holder
+        for(spice* j : SpiceHolder){
+            //if there is a new greatest unit price
+            if(GUnitPrice == nullptr || j->unitPrice > GUnitPrice->unitPrice && j->Processed == false){
+                //make the GUnitPrice pointer equal to it
+                GUnitPrice = j;
+            }
         }
+
+        if (GUnitPrice->Processed == true) {
+            //no more spices left
+            break;
+        }
+
+        //will set the spice of the greatest one to true so that if it is used up in full we do not check it
+        //of if its a fraction then we know we are done
+        GUnitPrice->Processed = true;
+
+        if(GUnitPrice->QTY <= NapSize){
+
+            //will handle whole amounts
+
+            NapSize = NapSize - GUnitPrice->QTY;
+            total = total + GUnitPrice->price;
+
+
+        }else if(GUnitPrice->QTY > NapSize){
+
+            //will handle fractional amounts
+
+            total = total + GUnitPrice->unitPrice * NapSize;
+            //cout << "Knapsack of capacity " << std::to_string(trueSize) << " is worth " << std::to_string(total) << " quatloos and contains " << std::to_string(NapSize) << " scoop of " << GUnitPrice->name << "." << "\n";
+            break;
+            
+        }
+
     }
 
-    //not getting to the right function to do the work look at geeks for geeks for that answer
+    //resets all the spice values Prcosessed values to false so that it can be done again with a different knap sack
+    for(spice* i : SpiceHolder){
+        i->Processed = false;
+    }
     
-    if(GUnitPrice->QTY < NapSize){
-
-        //will handle whole amounts
-
-        NapSize = NapSize - GUnitPrice->QTY;
-        total = GUnitPrice->price;
-
-    }else if(GUnitPrice->QTY > NapSize){
-
-        //will handle fractional amounts
-
-        cout << "Knapsack of capacity " << std::to_string(NapSize) << " is worth " << std::to_string(GUnitPrice->unitPrice * NapSize) << " quatloos and contains " << std::to_string(NapSize) << " scoop of " << GUnitPrice->name << "." << "\n";
-        
-    }
 }
 
 
--- Assignment4/main.cpp
+++ Assignment4/main.cpp
@@ -55,54 +55,79 @@ int main(){
     //will hold the knapsacks value
     vector<int> kanpsacks;
 
+    //start of the long amount of logic that is used to correctly process the file
     for(string i: SpiceHeist){
+
+        //checks for comments
         if(i.find("--") == std::string::npos){
+            //the word spice it found then we know that there is a new spice
             if(i.find("spice") != std::string::npos){
                 std::istringstream iss(i);
                 std::string token;
 
                 while (iss >> token) {
+                    //then we skip over all the words and symbols so that we are left with just the values
                     if (token == "spice" || token == "name" || token == "=" || token == "total_price" || token == "qty") {
                         continue;
                     }
+                    //this only works is the values stay in the same format
 
+                    //the first thing is always the spice name so we find the token with the semi colon
                     if(token.find(";") != std::string::npos && count == 0){
+                        //make that token the SpiceName
                         SpiceName = token;
+                        //trim the semi colon off
                         SpiceName.erase(remove(SpiceName.begin(), SpiceName.end(), ';'), SpiceName.end());
+                        //make the count one more so that we can move on to the next thing to add
                         count++;
                         continue;
                     }
 
+                    //the second thing is always the SpicePrice
                     if(token.find(";") != std::string::npos && count == 1){
+                        //since this will be a float we have to get ride of the semi colon first before converting from string to float
                         token.erase(remove(token.begin(), token.end(), ';'), token.end());
+                        //convert
                         SpicePrice = stof(token);
+                        //move the count along to check for the next thing
                         count++;
                         continue;
                     }
 
+                    //the third this will always be the SpiceQTY
                     if(token.find(";") != std::string::npos && count == 2){
+                        //since this wil be a int we have the trim the semi colon off first before converstion
                         token.erase(remove(token.begin(), token.end(), ';'), token.end());
+                        //convertion
                         SpiceQty = stoi(token);
+                        //set the count back to look for the SpiceName since we have all the data for the spice after this
                         count = 0;
                         continue;
                     }
             
                 }
 
+                //then pass that data to Spice function in "FractionalGreedy.hpp" to create the spice
                 Spice(SpiceName, SpicePrice, SpiceQty);
 
+            //if we see a knapsack word do other actions
             }else if(i.find("knapsack") != std::string::npos){
                 std::istringstream iss(i);
                 std::string token;
 
+                //much less to skip over
                 while (iss >> token) {
                     if (token == "knapsack" || token == "capacity" || token == "=") {
                         continue;
                     }
 
-                    if(token.find(";") != std::string::npos){
+                    //same logic as before once the token have a semicolon at it thats the data
+                    if(token.find(";") != std::string::npos){   
+                        //trim the semi colon off since the knap sack size will be a int
                         token.erase(remove(token.begin(), token.end(), ';'), token.end());
+                        //convert
                         knapsackSize = stoi(token);
+                        //add to the vector that will hold all the knap sack sizes
                         kanpsacks.push_back(knapsackSize);
                     }
                 }
@@ -111,6 +136,7 @@ int main(){
         }
     }
 
+    //this wil go through all the knap sack sizes and send them over to the FractionGreedy algorithm in "FractionalGreedy.hpp
     for(int i : kanpsacks){
         FractionalGreedy(i);
     }
